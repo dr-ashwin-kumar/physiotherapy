bad297a6fda60a28623c0878a4bf288d5e225018
dr-ashwin-kumar <drashwinkumar09@gmail.com>
2025-06-15 14:06:28 +0530

    https://github.com/dr-ashwin-kumar/Physiotherapy-.git

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sri Sai Balaji Physiotherapy Clinic</title>
+    <link rel="icon" href="./assets/images/favicon.png" type="icon/x-image">
+
+    <!-- link for the style -->
+    <link rel="stylesheet" href="./style.css">
+
+    <!-- links for the fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Philosopher:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
+    <!-- links for the icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <!-- start of the top navbar -->
+    <div class="top-nav">
+        <div class="topnav-mail-location">
+            <ul>
+                <li><a href="#"><i class="fa-solid fa-envelope"></i><span>&nbsp; srisaibalajiclinic@gmail.com</span></a></li>
+                <li><a href="#"><i class="fa-solid fa-location-dot"></i><span>&nbsp; Thindlu, Bengaluru 560097</span></a></li>
+            </ul>
+        </div>
+        <div class="topnav-soc-links">
+            <ul>
+                <li><a href="#"><i class="fa-solid fa-phone"></i><span>&nbsp; +91 96633 14071 </span></a></li>
+                <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- start of the navbar -->
+    <div class="navbar">
+        <div class="navbar-logo">
+            <li><a href="#"><img src="./assets/images/logo.png" alt="logo"></a></li>
+        </div>
+        <div class="navbar-menu-items">
+            <ul>
+                <li><a href="./index.php">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Blog</a></li>
+            </ul>
+        </div>
+        <div class="navbar-button">
+            <ul>
+                <li><a href="#">Book Appointment</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+
+</html>
