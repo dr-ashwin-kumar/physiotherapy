13aa82623838d01b356a20351dec3e3fb0f69144
dr-ashwin-kumar <drashwinkumar09@gmail.com>
2025-06-22 23:04:39 +0530

    Updated carousel

--- index.php
+++ index.php
@@ -17,10 +17,15 @@
 
     <!-- links for the icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Swiper.js link for the carousel container -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+
 </head>
 
 <body>
-    <!-- start of the top navbar -->
+    <!-- -----------------------start of the top navbar------------------------- -->
     <div class="top-nav">
         <div class="topnav-mail-location">
             <ul>
@@ -37,9 +42,9 @@
             </ul>
         </div>
     </div>
-    <!-- start of the navbar -->
+    <!-- -------------------start of the navbar----------------------- -->
     <div class="navbar">
-        <div class="navbar-logo">
+        <div class="navbar-logo">`
             <li><a href="#"><img src="./assets/images/logo.png" alt="logo"></a></li>
         </div>
         <div class="navbar-menu-items">
@@ -57,6 +62,180 @@
             </ul>
         </div>
     </div>
+
+    <!-- -------------start of the sidebar-------------------- -->
+    <div class="sidebar">
+        <div class="sidebar-logo">
+            <ul>
+                <li><a href="#"><img src="./assets/images/logo.png" alt=""></a></li>
+                <li class="hamburger"><i class="fa-solid fa-bars"></i></li>
+            </ul>
+        </div>
+        <div class="sidebar-menu-items">
+            <ul>
+                <li><a href="./index.php">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Blog</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <!-- ---------------------start of the carousel container----------------- -->
+    <!-- MAIN CAROUSEL -->
+    <div class="carousel-container mySwiper2 swiper">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <img src="./assets/images/index-carousel/slide-1.jpg" alt="">
+                <div class="overlay-image">
+                    <img src="./assets/images/index-carousel/grey-shadow.png" alt="">
+                </div>
+                <div class="slide-content">
+                    <h3>Advice About Orthotics</h3>
+                    <h1>Physiotherapy, anterior knee pain</h1>
+                    <p>Use the latest research in physical therapy to work better, play better—and spend more time on what really matters.</p>
+                    <div class="content-buttons">
+                        <div class="button">
+                            <a href="#">Book</a>
+                        </div>
+                        <div class="content-video">
+                            <h4>See Our Video</h4>
+                            <a href="#"><span><i class="fa-brands fa-youtube"></i></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <img src="./assets/images/index-carousel/slide-2.jpg" alt="">
+                                <div class="overlay-image">
+                    <img src="./assets/images/index-carousel/grey-shadow.png" alt="">
+                </div>
+                <div class="slide-content">
+                    <h3>Advice About Orthotics</h3>
+                    <h1>Physiotherapy, anterior knee pain</h1>
+                    <p>Use the latest research in physical therapy to work better, play better—and spend more time on what really matters.</p>
+                    <div class="content-buttons">
+                        <div class="button">
+                            <a href="#">Book </a>
+                        </div>
+                        <div class="content-video">
+                            <h4>See Our Video</h4>
+                            <a href="#"><span><i class="fa-brands fa-youtube"></i></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <img src="./assets/images/index-carousel/slide-3.jpg" alt="">
+                                <div class="overlay-image">
+                    <img src="./assets/images/index-carousel/grey-shadow.png" alt="">
+                </div>
+                <div class="slide-content">
+                    <h3>Advice About Orthotics</h3>
+                    <h1>Physiotherapy, anterior knee pain</h1>
+                    <p>Use the latest research in physical therapy to work better, play better—and spend more time on what really matters.</p>
+                    <div class="content-buttons">
+                        <div class="button">
+                            <a href="#">Book </a>
+                        </div>
+                        <div class="content-video">
+                            <h4>See Our Video</h4>
+                            <a href="#"><span><i class="fa-brands fa-youtube"></i></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <img src="./assets/images/index-carousel/slide-4.jpg" alt="">
+                                <div class="overlay-image">
+                    <img src="./assets/images/index-carousel/grey-shadow.png" alt="">
+                </div>
+                <div class="slide-content">
+                    <h3>Advice About Orthotics</h3>
+                    <h1>Physiotherapy, anterior knee pain</h1>
+                    <p>Use the latest research in physical therapy to work better, play better—and spend more time on what really matters.</p>
+                    <div class="content-buttons">
+                        <div class="button">
+                            <a href="#">Book </a>
+                        </div>
+                        <div class="content-video">
+                            <h4>See Our Video</h4>
+                            <a href="#"><span><i class="fa-brands fa-youtube"></i></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Navigation Buttons -->
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+    </div>
+
+    <!-- THUMBNAILS -->
+    <div class="thumbnail-container mySwiper swiper">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <img src="./assets/images/index-carousel/slide-1.jpg" />
+            </div>
+            <div class="swiper-slide">
+                <img src="./assets/images/index-carousel/slide-2.jpg" />
+            </div>
+            <div class="swiper-slide">
+                <img src="./assets/images/index-carousel/slide-3.jpg" />
+            </div>
+            <div class="swiper-slide">
+                <img src="./assets/images/index-carousel/slide-4.jpg" />
+            </div>
+        </div>
+    </div>
+
+    
+
+    <!-- ---------------script for the sidebar------------- -->
+    <script>
+        const hamburger = document.querySelector('.hamburger');
+        const menuList = document.querySelector('.sidebar-menu-items');
+
+        // Make sure it's closed by default
+        menuList.classList.remove('open');
+
+        hamburger.addEventListener('click', () => {
+            menuList.classList.toggle('open');
+        });
+    </script>
+
+
+    <!-- ------------script for the carousel container----------- -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+    <!-- Initialize Swiper -->
+    <script>
+        const thumbnailSwiper = new Swiper(".mySwiper", {
+            spaceBetween: 10,
+            slidesPerView: 4,
+            freeMode: true,
+            watchSlidesProgress: true,
+            breakpoints: {
+            768: { slidesPerView: 4 },
+            480: { slidesPerView: 4 },
+            320: { slidesPerView: 4 },
+            },
+        });
+
+        const mainSwiper = new Swiper(".mySwiper2", {
+            spaceBetween: 10,
+            loop: true,
+            navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+            },
+            thumbs: {
+            swiper: thumbnailSwiper,
+            },
+        });
+    </script>
+
 </body>
 
 </html>
